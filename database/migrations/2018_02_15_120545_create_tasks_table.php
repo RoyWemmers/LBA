a667ef8e8f88ff48adae48cc10dec3c78b680924
Unknown <ro.wemmers@gmail.com>
2018-02-15 13:51:02 +0100

    Added Migration

---
+++ database/migrations/2018_02_15_120545_create_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('taskId');
+            $table->string('taskBody');
+            $table->integer('taskCreator');
+            $table->date('taskCreationDate');
+            $table->string('taskCategory');
+            $table->time('taskStartTime');
+            $table->time('taskEndTime');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
